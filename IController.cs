0eb9b18ea062218d953e60f1da24a4e728482de8
Sam Espanioly <espanioly.1@buckeyemail.osu.edu>
2024-01-26 13:34:29 -0500

    Added ISprite and IController, I am pro at github now!

---
+++ IController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyFirstGame
+{
+    internal interface IController
+    {
+        public void Update()
+        { }
+        public void RegisterCommand(Keys key, ICommand command)
+        { }
+        }
+}
---
+++ ISprite.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGame
+{
+    public interface ISprite
+    { 
+        public void Update() { }
+        public void Draw(SpriteBatch sprite, Vector2 vec);
+    }
+}
